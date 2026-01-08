2ccc189db275ff07f04f3b9f1582447e3e944a98
DarbyCai <gilber2314@outlook.com>
2026-01-08 17:44:42 +0800

    chore: Upload assignment 3-1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,11 @@
 #include "systemcalls.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -9,15 +16,22 @@
 */
 bool do_system(const char *cmd)
 {
-
 /*
  * TODO  add your code here
  *  Call the system() function with the command set in the cmd
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+		int ret = system(cmd);
+		if (ret == -1 || ret == 127) {
+			return false;
+		}
 
-    return true;
+		if (ret == 0) {
+			return true;
+		}
+
+		return false;
 }
 
 /**
@@ -58,8 +72,24 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+		pid_t pid;
+		pid = fork();
+		if (pid == -1) {
+			perror("fork()");
+			exit(EXIT_FAILURE);
+		} else if (pid == 0) {
+			if (execv(command[0], command) == -1) {
+				perror("execv");
+				exit(EXIT_FAILURE);
+			}
+		} else {
+			int status;
+			wait(&status);
+			return !status ? true : false;
+		}
 
     va_end(args);
+
 
     return true;
 }
@@ -84,7 +114,6 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     // and may be removed
     command[count] = command[count];
 
-
 /*
  * TODO
  *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
@@ -92,6 +121,34 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+		pid_t pid;
+		int fd = open("outputfile", O_WRONLY|O_TRUNC|O_CREAT, 0644); 
+		if (fd == -1) {
+			perror("open()");
+			exit(EXIT_FAILURE);
+		}
+
+		if (dup2(fd, 1) < 0) {
+			perror("dup2");
+			exit(EXIT_FAILURE);					
+		}
+		close(fd);
+
+		pid = fork();
+		if (pid == -1) {
+			perror("fork()");
+			exit(EXIT_FAILURE);
+		} else if (pid == 0) {
+			if (execv(command[0], command) == -1) {
+				perror("execv");
+				exit(EXIT_FAILURE);
+			}
+		} else {
+			int status;
+			close(fd);
+			wait(&status);
+			return !status ? true : false;
+		}
 
     va_end(args);
 
